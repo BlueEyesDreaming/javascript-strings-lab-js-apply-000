7a6d99bde2b02cc4085b205d25caae4688e20765
Ashton Reed <dreamergirl411@gmail.com>
2017-12-23 23:24:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
@@ -7,7 +7,3 @@ var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "space";
 
 var conversation = `${topic}`;
-
- function greeting() {
-   return 'Hello, everybody!'
- }
